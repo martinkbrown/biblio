183c0bd9026bac5ec25a1e8e0cb8be205c260c53
sherene.campbell@gmail.com <sherene.campbell@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-03-29 20:17:06 +0000

    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@35 ab7f3f59-4413-453d-a97f-7e6563fd93ca

--- Development/conference_meeting_toc.php
+++ Development/conference_meeting_toc.php
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 require_once 'header.php';
-
+$conf_id = $_GET['big_conf'];
+$conf_meet_id = $_GET['conference_meeting_id'];
+echo "<h2>Name of Conference</h2>";
+echo '<a href="conference_paper.php?big_conf='.$conf_id.'&conf_meet_id='.$conf_meet_id.'">Add new Conference Paper</a>';
 ?>
-<h2>Name of Conference</h2>
 <h3>Conference Meeting (e.g.proceedings of ...)City, State, Country, Month Days, Year, Publisher Year</h3>
