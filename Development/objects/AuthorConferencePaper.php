948ca82db136e17db6db6daad326dceb0ac72e55
martijnetje@gmail.com <martijnetje@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-03-30 01:03:24 +0000

    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@39 ab7f3f59-4413-453d-a97f-7e6563fd93ca

--- Development/objects/AuthorConferencePaper.php
+++ Development/objects/AuthorConferencePaper.php
@@ -11,14 +11,12 @@
  */
 class AuthorConferencePaper extends Recordset
 {
-    function AuthorConferencePaper()
-    {
-        parent::Recordset(0,"author_conference_paper");
-    }
-
     function AuthorConferencePaper($author_id,$paper_id)
     {
         $query = "SELECT * FROM author_conference_paper WHERE author_id = '$author_id' AND conference_paper_id = '$paper_id'";
+
+        parent::Recordset(0,"author_conference_paper");
+
         $this->loadByQuery($query);
     }
 }
