c0927d85c3af14311ff7674b41a74c951d087658
martijnetje@gmail.com <martijnetje@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-04-06 15:25:38 +0000

    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@65 ab7f3f59-4413-453d-a97f-7e6563fd93ca

---
+++ Development/objects/AuthorJournalPaper.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AuthorJournalPaper
+ *
+ * @author martin
+ */
+class AuthorJournalPaper extends Recordset
+{
+    function AuthorJournalPaper($author_id,$paper_id)
+    {
+        $query = "SELECT * FROM author_journal_paper WHERE author_id = '$author_id' AND journal_paper_id = '$paper_id'";
+
+        parent::Recordset(0,"author_journal_paper");
+
+        $this->loadByQuery($query);
+    }
+}
+?>
--- Development/objects/JournalPaper.php
+++ Development/objects/JournalPaper.php
@@ -13,26 +13,100 @@ class JournalPaper extends Recordset
 {
     /**
      *
-     * @var string  The main query for retrieving conference meetings
+     * @var string  The main query for retrieving journal meetings
      */
-    var $query = "SELECT cm.id, cm.name, cm.city, s.id as state_id, s.name as state_name, s.abbreviation as state_code,
-                                country.id as country_id, country.name as country_name, cm.publisher_website,
-                                cm.conference_website, cm.isbn,
-                                cm.start_date, cm.end_date, cm.approved, p.id as publisher_id,
-                                p.name as publisher_name,
-                                c.name as conference_name
-                                FROM (conference_meeting cm, conference c, country)
-                                LEFT JOIN state s ON cm.state_id = s.id
-                                LEFT JOIN publisher p ON cm.publisher_id = p.id
-                                WHERE c.approved = 1 AND cm.country_id = country.id AND cm.conference_id = c.id ";
+    var $query = "SELECT jp.id, jp.title, jp.start_page, jp.end_page, jp.create_date, jp.email, jp.approved, jp.volume, jp.number,
+                                jvn.date as `date`, j.name as source_name
+                                FROM (journal_paper jp, author a, author_journal_paper ajp, journal j)
+                                LEFT JOIN journal_volume_number jvn ON (jp.number = jvn.number AND jp.volume = jvn.volume AND jp.journal_id = jvn.journal_id)
+                                WHERE jp.id = ajp.journal_paper_id AND a.id = ajp.author_id AND j.id = jp.journal_id  ";
+
+        /**
+     *
+     * @var array   An array containing the authors of this journal paper
+     */
+    var $authors = array();
+
+    var $orderBy = " cm.start_date DESC";
 
     /**
-     * You should use the methods to load the conference meetings instead
+     * You should use the methods to load the journal meetings instead
      *
      */
     function JournalPaper($id=0)
     {
-        parent::Recordset($id,"journal_paper");
+        parent::Recordset($this->query . " AND jp.id = '$id'","journal_paper");
+    }
+
+    function getJournalPapersByAuthorId($author_id)
+    {
+        $this->query .= " AND a.id = '$author_id'";
+
+        $this->loadByQuery($this->query . " ORDER BY " . $this->orderBy);
+    }
+
+    /**
+     *
+     * @param Author $author    An Author object. Should contain the information about one of the paper's authors
+     */
+    function addAuthor($author)
+    {
+        array_push($this->authors,$author);
+    }
+
+    /**
+     *
+     * @return bool     You should use the save() function
+     */
+    function insert()
+    {
+        if(parent::insert())
+        {
+            $this->saveAuthors();
+        }
+
+        else return false;
+
+        //return $this->cs->save();
+    }
+
+    /**
+     *
+     * @return bool     You should use the save() function
+     */
+    function update()
+    {
+        if(parent::update())
+        {
+            $this->saveAuthors();
+        }
+
+        else return false;
+
+        //return $this->cs->save();
+    }
+
+    /**
+     * You should not be calling this explicitly. It will be called when you save this JournalPaper
+     */
+    function saveAuthors()
+    {
+        $main_author = true;
+
+        foreach($this->authors as $author)
+        {
+            $author->save();
+
+            $ajp = new AuthorJournalPaper($author->getId(),$this->getId());
+
+            $ajp->setValue('author_id',$author->getId());
+            $ajp->setValue('journal_paper_id',$this->getId());
+            $ajp->setValue('main_author',$main_author ? 1 : 0);
+
+            $main_author = false;
+
+            $acp->save();
+        }
     }
 }
 ?>
