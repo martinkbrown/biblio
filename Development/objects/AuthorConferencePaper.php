06bf771cb9c1767a382da518b8df528a5a986f5d
martijnetje@gmail.com <martijnetje@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-04-11 11:43:25 +0000

    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@93 ab7f3f59-4413-453d-a97f-7e6563fd93ca

--- Development/objects/AuthorConferencePaper.php
+++ Development/objects/AuthorConferencePaper.php
@@ -13,7 +13,10 @@ class AuthorConferencePaper extends Recordset
 {
     function AuthorConferencePaper($author_id,$paper_id)
     {
-        $query = "SELECT * FROM author_conference_paper WHERE author_id = '$author_id' AND conference_paper_id = '$paper_id'";
+        $author_id = (int) $author_id;
+        $paper_id = (int) $paper_id;
+
+        $query = "SELECT * FROM author_conference_paper WHERE author_id = $author_id AND conference_paper_id = $paper_id";
 
         parent::Recordset(0,"author_conference_paper");
 
--- Development/objects/AuthorJournalPaper.php
+++ Development/objects/AuthorJournalPaper.php
@@ -15,6 +15,7 @@ class AuthorJournalPaper extends Recordset
     {
         $author_id = (int) $author_id;
         $paper_id = (int) $paper_id;
+
         $query = "SELECT * FROM author_journal_paper WHERE author_id = $author_id AND journal_paper_id = $paper_id";
 
         parent::Recordset(0,"author_journal_paper");
