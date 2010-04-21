3fdd170e734d17ef7e6da83978dc3a7463ccaecd
sherene.campbell@gmail.com <sherene.campbell@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-04-21 02:33:10 +0000

    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@134 ab7f3f59-4413-453d-a97f-7e6563fd93ca

--- Development/conference_meeting_toc.php
+++ Development/conference_meeting_toc.php
@@ -18,10 +18,12 @@ require_once OBJECTS . 'Author.php';
 
 $conf_meeting = new ConferenceMeeting();
 $conf_paper = new ConferencePaper();
+$no_session_paper = new ConferencePaper();
 $nodataconference = true;
 $author = new Author();
 $conf_meet_id = (int)$_GET['conference_meeting_id'];
 $conf_meeting->loadConferenceMeetingById($conf_meet_id);
+$zero = 0;
 
 echo '<h2>'.  $conf_meeting->_conference_name  .'('.$conf_meeting->_conference_acronym.')'.'</h2>';
 echo '<a href="conference_paper.php?big_conf='.$conf_id.'&conf_meet_id='.$conf_meet_id.'">Add new Conference Paper </a>';
@@ -35,9 +37,7 @@ $country = $conf_meeting->_country_name;
 $publisher = $conf_meeting->_publisher_name;
 $isbn = $conf_meeting->isbn;
 
-function print_toc($conf_session_id,$conf_meet_id,$conf_session_id,$author,$conf_paper,$conf_session,$conf_paper){
-
-          echo '<tr><td>'.$conf_session->name.'</td></tr>';
+function print_toc($conf_meet_id,$author,$conf_paper){
           do{
               echo '<tr><td></td><td>'.$conf_paper->title.'</td></tr>';
               $id_paper=$conf_paper->id;
@@ -63,24 +63,38 @@ if ($year != $year2)
     $year = $year. " - " . $year2;
 
 echo "<h3>$name: $daterange $city,  $state, $country; $publisher  ISBN#:$isbn </h3>";
-
 echo '<table>';
+// print the papers that do no belong to conference session
+$query = "SELECT cp.id,cp.title,cp.start_page,cp.end_page from conference_paper cp where cp.conference_meeting_id=$conf_meet_id and conference_session_id = $zero";
+$no_session_paper->loadByQuery($query);
+do
+{
+          if ($no_session_paper->id){
+              echo '<tr><td>'."NO SESSION".'</td></tr>';
+              print_toc($conf_meet_id,$author,$no_session_paper);
+              $nodataconference = false;
+          }
+
+}while($no_session_paper->next());
+
+
 do
 {
       $conf_session_id = (int) $conf_session->id;
 
-      if ($conf_session_id){$conf_paper_id = "SELECT cp.id,cp.title,cp.start_page,cp.end_page from conference_paper cp
-      left join conference_session cs on cs.id = cp.conference_session_id
-      where cp.conference_meeting_id=$conf_meet_id and cp.conference_session_id = $conf_session_id ";}
-      else {
-          $conf_paper_id =  "SELECT cp.id,cp.title,cp.start_page,cp.end_page from conference_paper cp
-          where cp.conference_meeting_id=$conf_meet_id" ;
-      }
-      $conf_paper->loadByQuery($conf_paper_id);
-      if ($conf_paper->id){
-          print_toc($conf_session_id,$conf_meet_id,$conf_session_id,$author,$conf_paper,$conf_session,$conf_paper);
-          $nodataconference = false;
-      }
+          if ($conf_session_id){$conf_paper_id = "SELECT cp.id,cp.title,cp.start_page,cp.end_page from conference_paper cp
+          left join conference_session cs on cs.id = cp.conference_session_id
+          where cp.conference_meeting_id=$conf_meet_id and cp.conference_session_id = $conf_session_id ";}
+          else {
+              $conf_paper_id =  "SELECT cp.id,cp.title,cp.start_page,cp.end_page from conference_paper cp
+              where cp.conference_meeting_id=$conf_meet_id" ;
+          }
+          $conf_paper->loadByQuery($conf_paper_id);
+          if ($conf_paper->id){
+              echo '<tr><td>'.$conf_session->name.'</td></tr>';
+              print_toc($conf_meet_id,$author,$conf_paper);
+              $nodataconference = false;
+          }
 
 }while($conf_session->next());
 
