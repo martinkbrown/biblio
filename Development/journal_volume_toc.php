4e1d4fce6fc3a9fc6e5cc0fdbdb26f038e213792
sihle.wilson@gmail.com <sihle.wilson@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-04-26 03:06:49 +0000

    Small updates to journal_volume_toc.php page to make things visible
    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@147 ab7f3f59-4413-453d-a97f-7e6563fd93ca

--- Development/journal_volume_toc.php
+++ Development/journal_volume_toc.php
@@ -12,17 +12,17 @@ require_once FRONT_END . OBJECTS . 'JournalVolumeNumber.php';
 $j_v_id = $_GET['journal_volume_id'];
 $journalVolumeNumber = new JournalVolumeNumber($j_v_id);
 $journal = new Journal($journalVolumeNumber->getValue('journal_id'));
-
 $j_name_display = $journal->getValue("name");
 ?>
 
-<h1><?php echo $j_name_display?></h1>
+<div class ="conf_back">
+    <span class="solid_writting"><?php echo ($j_name_display); ?> </span> <br>
 
-<a href="submit_journal.php">Click here to add a paper to a</a><br><br><br>
-
-
+    <br>
 <?php
-
+// Printing out Volumes for JournalVolume Selected
+$grid = new JournalVolumeGrid();
 
 
 ?>
+</div>
