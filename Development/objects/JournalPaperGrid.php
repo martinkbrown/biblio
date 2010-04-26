4fc5953d40b9b7b797a336e994b8cc1d75bb2d64
martijnetje@gmail.com <martijnetje@gmail.com@ab7f3f59-4413-453d-a97f-7e6563fd93ca>
2010-04-26 13:45:31 +0000

    JournalPaperGrid for displaying on the Journal TOC page
    git-svn-id: https://free1.projectlocker.com/CEN5015Spring2010/ResearchDatabase/svn@152 ab7f3f59-4413-453d-a97f-7e6563fd93ca

---
+++ Development/objects/JournalPaperGrid.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+*/
+require_once LIB . 'Date.php';
+/**
+ * Description of JournalVolumeGrid
+ *
+ * @author Sihle
+ */
+class JournalPaperGrid extends Grid {
+
+    function handle_volume($row) {
+        print_r($row);
+    }
+}
+?>
